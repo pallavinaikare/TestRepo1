041bbf76aea7727fc22c52788284972e82d4a62a
Pallavi Naikare <ms.pallavipatil@gmail.com>
2022-01-23 09:07:59 +0530

    B2 Changes

--- src/P1/Test1.java
+++ src/P1/Test1.java
@@ -3,6 +3,7 @@ package P1;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("In p1-Test1");
+		System.out.println("P1-P7_line 6 Fram B2");
 	}
 
 }
--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -3,6 +3,7 @@ package p2;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("In P2-Test2");
+		System.out.println("Hello");
 	}
 
 
